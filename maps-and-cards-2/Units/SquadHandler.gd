e05f380b2e6e69f184bdfbc09d5dc29d134f4e4c
GaelleBescond <gaellebescondg@gmail.com>
2026-08-06 04:17:12 +0200

    ADHD push
    ADHD stuff i don't remember what I did
    To do:
    Get units inside area
    Create move order by selecting areas
    Check if squad of another team is inside

--- maps-and-cards-2/Units/SquadHandler.gd
+++ maps-and-cards-2/Units/SquadHandler.gd
@@ -1,21 +1,30 @@
 extends CharacterBody3D
 
-@export var squad_speed := 5.0
+@export var squadSpeed := 5.0
+@export var player := 0
+var playerOwner : int :
+	set (ownerchange):
+		if label_3d != null:
+			var color: Color
+			label_3d.text = "Player" + str(player)
+			if ownerchange == 1:
+				color = Color.DARK_BLUE
+				label_3d.modulate = color
+			else:
+				if ownerchange == 2:
+					color = Color.DARK_RED
+					label_3d.modulate = color
 var originPosition : = Vector3()
 var targetPosition : = Vector3(0,0,0)
-var moveOrder := false:
-	set(hasMoveOrder):
-		print("Move order :" + str(hasMoveOrder))
-var isInCombat := false:
-	set(combat):
-		print("in combat :" + str(combat))
+var moveOrder := false
+var isInCombat := false
 
-
+@onready var label_3d: Label3D = $Label3D
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
 
 func _ready() -> void:
-	moveOrder = true
-
+	playerOwner = player
+	received_move_order(targetPosition)
 
 
 func _physics_process(delta: float) -> void:
@@ -23,7 +32,7 @@ func _physics_process(delta: float) -> void:
 		var destination = navigation_agent_3d.get_next_path_position()
 		var local_destinaton = destination - global_position
 		var direction = local_destinaton.normalized()	
-		velocity = direction * squad_speed
+		velocity = direction * squadSpeed
 		move_and_slide()
 
 func received_move_order(targetPosition) -> void:
@@ -32,9 +41,8 @@ func received_move_order(targetPosition) -> void:
 
 
 func _on_navigation_agent_3d_target_reached() -> void:
-	if !isInCombat:
-		if !moveOrder:
-			originPosition = targetPosition
+	moveOrder = false
+	originPosition = targetPosition
 
 func retreat() -> void:
 	pass
--- maps-and-cards-2/area of control/base_area_of_control.gd
+++ maps-and-cards-2/area of control/base_area_of_control.gd
@@ -1,25 +1,52 @@
 extends Area3D
 
-@export var Owner := "Player 0"
-@export var capture_gauge := 1.0
-@onready var csg_cylinder_3d: CSGCylinder3D = $CSGCylinder3D
+@export var player := 0
+@export var captureGauge := 100.0
+var squadsInside : Array[Node] = []
+var playerOwner : int :
+	set (ownerchange):
+		var color: Color
+		owner_ui.text = "Player" + str(player)
+		if ownerchange == 1:
+			color = Color.DARK_BLUE
+			owner_ui.modulate = color
+		else:
+			if ownerchange == 2:
+				color = Color.DARK_RED
+				owner_ui.modulate = color
+
+@onready var area_unit_collider: CollisionShape3D = $area_unit_collider
+@onready var csg_combiner_3d: CSGCombiner3D = $CSGCombiner3D
+@onready var owner_ui: Label3D = $OwnerUI
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
+	print(get_overlapping_bodies())
+	for body in get_overlapping_bodies():
+		if body.is_in_group("Squad_Handler"):
+			
+			body.append(squadsInside)
+	print(squadsInside)
+	playerOwner = player
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_mouse_entered() -> void:
-	csg_cylinder_3d.visible = true
-	
-	pass # Replace with function body.
-
-
+	csg_combiner_3d.visible = true
 func _on_mouse_exited() -> void:
-	csg_cylinder_3d.visible = false
-	pass # Replace with function body.
+	csg_combiner_3d.visible = false
+
+
+func _on_timer_timeout() -> void:
+	pass
+
+func check_squads_inside() -> void:
+	pass
+
+
+func _on_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
+	squadsInside.append_array(body)
+	print(body_rid)
+	
