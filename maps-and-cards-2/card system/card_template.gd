366fcd0c0409791efc4bddf9078bddee191d4fda
GaelleBescond <gaellebescondg@gmail.com>
2026-08-08 17:37:21 +0200

    Added card and area placeholders
    To do:
    Work on adding hand card template (for the 3 hands)

---
+++ maps-and-cards-2/card system/card_template.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+
+func _on_area_3d_mouse_entered() -> void:
+	print("mouse entered")
+	pass # Replace with function body.
+
+
+func _on_area_3d_mouse_exited() -> void:
+	print("mouse exited")
+	pass # Replace with function body.
